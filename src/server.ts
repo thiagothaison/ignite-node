b6f884a8532bf1071a2764e169111249fee82569
Thiago Thaison <thiagothaison@gmail.com>
2022-07-20 10:12:03 -0300

    feat: configure swagger route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import SwaggerUi from "swagger-ui-express";
+
+import swaggerFile from "~/root/swagger.json";
 
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
 
 app.use(router);
 
