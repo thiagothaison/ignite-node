6fbabbe79023702ed65c0724dd0a6f1030d412a8
Thiago Thaison <thiagothaison@gmail.com>
2022-07-21 16:55:53 -0300

    feat(migration): create specifications table

---
+++ src/database/migrations/1658433246307-CreateSpecificationsTable.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSpecificationsTable1658433246307
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "specifications",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            length: "15",
+          },
+          {
+            name: "description",
+            type: "varchar",
+            length: "100",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("specifications");
+  }
+}
