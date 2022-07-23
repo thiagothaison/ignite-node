098cbd061937cd3b17625cc1b4b3c00a18558d01
Thiago Thaison <thiagothaison@gmail.com>
2022-07-23 11:47:11 -0300

    feat: use jsonErrors middleware

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,10 @@ import "./shared/container";
 import swaggerFile from "~/root/swagger.json";
 
 import express from "express";
+import "express-async-errors";
 import SwaggerUi from "swagger-ui-express";
 
+import { jsonErrors } from "./middlewares/jsonErrors";
 import { router } from "./routes";
 
 const HTTP_PORT = process.env.HTTP_PORT || 3000;
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use("/docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(jsonErrors);
 
 app.listen(HTTP_PORT, () =>
   console.log(`🔥 Server started at http://localhost:${HTTP_PORT}`)
