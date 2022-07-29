92c81475094273c7f3dfe44d5337fccf69657b33
Thiago Thaison <thiagothaison@gmail.com>
2022-07-29 14:55:30 -0300

    feat(migration): create specifications_cars table

---
+++ src/infra/typeorm/migrations/1659097745237-CreateSpecificationsCarsTable.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSpecificationsCarsTable1659097745237
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "specifications_cars",
+        columns: [
+          {
+            name: "car_id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "specification_id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ["specification_id"],
+            referencedTableName: "specifications",
+            referencedColumnNames: ["id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+          {
+            columnNames: ["car_id"],
+            referencedTableName: "cars",
+            referencedColumnNames: ["id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable("specifications_cars");
+
+    const promises = table.foreignKeys.map(async (foreignKey) => {
+      await queryRunner.dropForeignKey("specifications_cars", foreignKey);
+    });
+
+    await Promise.all(promises);
+
+    await queryRunner.dropTable("specifications_cars");
+  }
+}
