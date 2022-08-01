9aa550204fbe59327a17854897779e19028648f9
Thiago Thaison <thiagothaison@gmail.com>
2022-08-01 20:49:21 -0300

    feat(domain): create-car-image use-case

---
+++ src/domain/use-cases/car-image/create-car-images.ts
@@ -0,0 +1,34 @@
+import { inject, injectable } from "tsyringe";
+
+import { CreateCarImage } from "@domain/contracts/dtos/car-image/create-car-image";
+import { ICarRepository } from "@domain/contracts/repositories/car";
+import { ICarImageRepository } from "@domain/contracts/repositories/car-image";
+import { AppError } from "@domain/errors/app-error";
+
+type Input = {
+  carId: string;
+  images: string[];
+};
+
+@injectable()
+class CreateCarImageUseCase {
+  constructor(
+    @inject("CarRepository") private carRepository: ICarRepository,
+    @inject("CarImageRepository")
+    private carImageRepository: ICarImageRepository
+  ) {}
+
+  async execute({ carId, images }: Input): CreateCarImage.Output {
+    const car = await this.carRepository.findById(carId);
+
+    if (!car) {
+      throw new AppError("Car does not exists", 400);
+    }
+
+    images.forEach(async (image) => {
+      await this.carImageRepository.create({ carId, image });
+    });
+  }
+}
+
+export { CreateCarImageUseCase };
