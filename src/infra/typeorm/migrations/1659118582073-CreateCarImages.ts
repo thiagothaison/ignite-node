5872fb9befa79e76f7fd0a0927210308bcd2e933
Thiago Thaison <thiagothaison@gmail.com>
2022-08-01 20:44:35 -0300

    feat(migration): create car_images table

---
+++ src/infra/typeorm/migrations/1659118582073-CreateCarImages.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCarImages1659118582073 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "car_images",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "car_id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "image",
+            type: "varchar",
+            isNullable: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ["car_id"],
+            referencedTableName: "cars",
+            referencedColumnNames: ["id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable("car_images");
+
+    const promises = table.foreignKeys.map(async (foreignKey) => {
+      await queryRunner.dropForeignKey("car_images", foreignKey);
+    });
+
+    await Promise.all(promises);
+
+    await queryRunner.dropTable("car_images");
+  }
+}
