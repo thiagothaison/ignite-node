a3185b2b31866fb109527d7232bbad41d7a97209
Thiago Thaison <thiagothaison@gmail.com>
2022-08-03 12:07:36 -0300

    refactor: deleteFile helper

--- src/domain/helpers/file.ts
+++ src/domain/helpers/file.ts
@@ -1,13 +1,16 @@
 import fs from "fs";
+import { resolve } from "path";
 
 const deleteFile = (filename: string) => {
+  const filenamePath = resolve(process.cwd(), filename);
+
   try {
-    fs.statSync(filename);
+    fs.statSync(filenamePath);
   } catch (err) {
     return;
   }
 
-  fs.unlinkSync(filename);
+  fs.unlinkSync(filenamePath);
 };
 
 export { deleteFile };
