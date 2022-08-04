bb0009690992f3bf4fb54d32657da31aa52f97d4
Thiago Thaison <thiagothaison@gmail.com>
2022-08-04 16:51:31 -0300

    refactor(rental): show only user logged rentals

--- src/application/controllers/rental/list.ts
+++ src/application/controllers/rental/list.ts
@@ -5,8 +5,12 @@ import { ListRentalsUseCase } from "@domain/use-cases/rental/list";
 
 class ListRentalController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const {
+      user: { id: userId },
+    } = request;
+
     const listRentalsUseCase = container.resolve(ListRentalsUseCase);
-    const rentals = await listRentalsUseCase.execute();
+    const rentals = await listRentalsUseCase.execute({ userId });
 
     return response.status(200).json(rentals);
   }
