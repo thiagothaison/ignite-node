8971498206c1f294c0826f6db6235f9b124a2a53
Thiago Thaison <thiagothaison@gmail.com>
2022-08-05 15:05:48 -0300

    fix(jest): setupFiles configuration

--- jest.config.ts
+++ jest.config.ts
@@ -138,7 +138,7 @@ export default {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ["<rootDir>/src/application/config/dotenv.ts"],
+  setupFiles: ["<rootDir>/src/config/dotenv.ts"],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
