f56ffff1862eda4a3c019dea971fe2c7a81f6f0a
Thiago Thaison <thiagothaison@gmail.com>
2022-08-09 17:37:50 -0300

    feat: create container for StorageProvider

--- src/infra/tsyringe/container/providers/index.ts
+++ src/infra/tsyringe/container/providers/index.ts
@@ -2,12 +2,22 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "@domain/contracts/providers/date-provider";
 import { IMailProvider } from "@domain/contracts/providers/mail";
+import { IStorageProvider } from "@domain/contracts/providers/storage";
 
 import { DayJsProvider } from "@infra/providers/date-provider/day-js-provider";
 import { EtherealProvider } from "@infra/providers/mail/ethereal";
+import { LocalStorageProvider } from "@infra/providers/storage/local";
+import { S3StorageProvider } from "@infra/providers/storage/s3";
 
 container.registerSingleton<IDateProvider>("DateProvider", DayJsProvider);
 container.registerInstance<IMailProvider>(
   "MailProvider",
   new EtherealProvider()
+);
+
+container.registerInstance<IStorageProvider>(
+  "StorageProvider",
+  process.env.NODE_ENV === "production"
+    ? new S3StorageProvider()
+    : new LocalStorageProvider()
 );
